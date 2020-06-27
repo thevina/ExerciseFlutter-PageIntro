c41ea90a6fd5b3d903d8040d2a86e6fe816844af
thevina <119877126+thevina@users.noreply.github.com>
2020-06-27 20:43:49 +0000

    Removed redundant comments

--- src/app/bots/abstract-bot.ts
+++ src/app/bots/abstract-bot.ts
@@ -15,4 +15,26 @@ export type Event =
       error: ChatError
     }
 
-export inte
+export interface SendMessageParams {
+  prompt: string
+  onEvent: (event: Event) => void
+  signal?: AbortSignal
+}
+
+export abstract class AbstractBot {
+  async sendMessage(params: SendMessageParams) {
+    try {
+      await this.doSendMessage(params)
+    } catch (err) {
+      console.error(err)
+      if (err instanceof ChatError) {
+        params.onEvent({ type: 'ERROR', error: err })
+      } else if (!params.signal?.aborted) {
+        // ignore user abort exception
+        params.onEvent({ type: 'ERROR', error: new ChatError((err as Error).message, ErrorCode.UNKOWN_ERROR) })
+      }
+    }
+  }
+
+  abstract doSendMessage(params: SendMessageParams): Promise<void>
+  abstract resetC
