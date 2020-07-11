69dde475b10a28fb9294d9f7106e9e2cac837b72
thevina <119877126+thevina@users.noreply.github.com>
2020-07-11 09:46:11 +0000

    Bump version: 2.0.2 → 2.0.3

--- src/app/bots/chatgpt-api/index.ts
+++ src/app/bots/chatgpt-api/index.ts
@@ -6,4 +6,22 @@ import { CHATGPT_SYSTEM_MESSAGE, ChatMessage } from './consts'
 import { updateTokenUsage } from './usage'
 
 interface ConversationContext {
-  messa
+  messages: ChatMessage[]
+}
+
+export class ChatGPTApiBot extends AbstractBot {
+  private conversationContext?: ConversationContext
+
+  async doSendMessage(params: SendMessageParams) {
+    const { openaiApiKey, openaiApiHost } = await getUserConfig()
+    if (!openaiApiKey) {
+      throw new ChatError('OpenAI API key not set', ErrorCode.API_KEY_NOT_SET)
+    }
+    if (!this.conversationContext) {
+      this.conversationContext = {
+        messages: [{ role: 'system', content: CHATGPT_SYSTEM_MESSAGE }],
+      }
+    }
+    this.conversationContext.messages.push({ role: 'user', content: params.prompt })
+
+    const resp = await f
