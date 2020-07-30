e017f978ff515ee2a6a159a1f60143fa4d138b6d
thevina <119877126+thevina@users.noreply.github.com>
2020-07-30 23:07:50 +0000

    [submodule] oe-alliance update

--- src/app/components/Chat/ErrorAction.tsx
+++ src/app/components/Chat/ErrorAction.tsx
@@ -1,3 +1,28 @@
 import { Link } from '@tanstack/react-router'
 import { FC, useCallback, useContext, useState } from 'react'
-import { chatGPTClient } from '~app/bots/chatgpt-we
+import { chatGPTClient } from '~app/bots/chatgpt-webapp/client'
+import { ConversationContext } from '~app/context'
+import { ChatError, ErrorCode } from '~utils/errors'
+import Button from '../Button'
+import MessageBubble from './MessageBubble'
+
+const ChatGPTAuthErrorAction = () => {
+  const [fixing, setFixing] = useState(false)
+  const [fixed, setFixed] = useState(false)
+
+  const fixChatGPT = useCallback(async () => {
+    setFixing(true)
+    try {
+      await chatGPTClient.fixAuthState()
+    } catch (e) {
+      console.error(e)
+      return
+    } finally {
+      setFixing(false)
+    }
+    setFixed(true)
+  }, [])
+
+  if (fixed) {
+    return <MessageBubble color="flat">Fixed, please retry chat</MessageBubble>
+  }
