c077dd8b364cc9fd4627aeedfc3eb02726b57a99
thevina <119877126+thevina@users.noreply.github.com>
2020-08-02 20:40:53 +0000

    Update plugin version

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,1 +1,15 @@
-import { Outlet
+import { Outlet } from '@tanstack/react-router'
+import Sidebar from './Sidebar'
+
+function Layout() {
+  return (
+    <div className="bg-[#6756BD] h-screen py-3 px-3">
+      <main className="grid grid-cols-[20%_1fr] h-full bg-[#ffffff66] rounded-[40px] max-w-[1316px] mx-auto backdrop-blur-2xl pl-5 py-4 pr-4">
+        <Sidebar />
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/app/components/Page.tsx
@@ -0,0 +1,3 @@
+import { FC, PropsWithChildren } from 'react'
+
+const PagePanel: FC<PropsWithChildren<{ title: string }>> = (props) => {
