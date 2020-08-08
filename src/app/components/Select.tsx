b4b15a2c7dcfdbd9d43d17459de420c89b3471c9
thevina <119877126+thevina@users.noreply.github.com>
2020-08-08 14:57:30 +0000

    little clean up

--- src/app/components/Select.tsx
+++ src/app/components/Select.tsx
@@ -1,2 +1,20 @@
 import cx from 'classnames'
-import { FC, Fr
+import { FC, Fragment, useMemo, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+interface Props<T> {
+  options: { value: T; name: string }[]
+  value: T
+  onChange: (value: T) => void
+}
+
+function Select<T extends string>(props: Props<T>) {
+  const { options, value, onChange } = props
+  const selectedName = useMemo(() => options.find((o) => o.value === value)!.name, [options, value])
+  return (
+    <Listbox value={value} onChange={onChange}>
+      {({ open }) => (
+        <>
+          <div className="relative">
+            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5
