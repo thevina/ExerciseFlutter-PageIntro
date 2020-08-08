f7d1ed4a87d75cc549074e66bc06514edd7e2033
thevina <119877126+thevina@users.noreply.github.com>
2020-08-08 20:49:53 +0000

    update TableCalculation

--- src/app/components/Select.tsx
+++ src/app/components/Select.tsx
@@ -17,4 +17,14 @@ function Select<T extends string>(props: Props<T>) {
       {({ open }) => (
         <>
           <div className="relative">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5
+            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              <span className="block truncate">{selectedName}</span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              lea
