b910eade2ce5f613768005d5bfbba093e57544d7
thevina <119877126+thevina@users.noreply.github.com>
2020-08-08 22:19:17 +0000

    Update hello.go

--- src/app/components/Select.tsx
+++ src/app/components/Select.tsx
@@ -27,4 +27,17 @@ function Select<T extends string>(props: Props<T>) {
               show={open}
               as={Fragment}
               leave="transition ease-in duration-100"
-              lea
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {options.map((option) => (
+                  <Listbox.Option
+                    key={option.value}
+                    className={({ active }) =>
+                      cx(
+                        active ? 'bg-[#6756BD] text-white' : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-3 pr-9',
+                      )
+                    }
+                    val
