1dc50660ffd46275c0037dcf1e9fc3cbf9704f20
thevina <119877126+thevina@users.noreply.github.com>
2020-08-15 19:32:30 +0000

    trim trailing whitespace

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -8,4 +8,21 @@ import cx from 'classnames'
 const Tabs = TabsPrimitive.Root
 
 const TabsList = React.forwardRef<
-  React.ElementRef<typeof Ta
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cx('inline-flex items-center justify-center rounded-md bg-slate-100 p-1 dark:bg-slate-800', className)}
+    {...props}
+  />
+))
+TabsList.displayName = TabsPrimitive.List.displayName
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    className={cx(
+      'inline-flex min-w-[100px] items-center justify-center rounded-[0.185re
