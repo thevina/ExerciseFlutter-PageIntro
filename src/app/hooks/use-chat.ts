8230cb5e6b17d14bd637876af006fc6807024e8b
thevina <119877126+thevina@users.noreply.github.com>
2020-08-22 19:35:44 +0000

    prepare for next version

--- src/app/hooks/use-chat.ts
+++ src/app/hooks/use-chat.ts
@@ -1,2 +1,24 @@
 import { useAtom } from 'jotai'
-import { useCallback, useMemo } from 're
+import { useCallback, useMemo } from 'react'
+import { chatFamily } from '~app/state'
+import { ChatMessageModel } from '~types'
+import { uuid } from '~utils'
+import { BotId } from '../bots'
+
+export function useChat(botId: BotId, page = 'singleton') {
+  const chatAtom = useMemo(() => chatFamily({ botId, page }), [botId, page])
+  const [chatState, setChatState] = useAtom(chatAtom)
+
+  const updateMessage = useCallback(
+    (messageId: string, updater: (message: ChatMessageModel) => void) => {
+      setChatState((draft) => {
+        const message = draft.messages.find((m) => m.id === messageId)
+        if (message) {
+          updater(message)
+        }
+      })
+    },
+    [setChatState],
+  )
+
+  const sendMessage = us
