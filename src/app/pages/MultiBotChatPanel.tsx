d62c27881a97bd9d5855db94ed1a00485c841e61
thevina <119877126+thevina@users.noreply.github.com>
2020-09-03 09:44:35 +0000

    Updated models

--- src/app/pages/MultiBotChatPanel.tsx
+++ src/app/pages/MultiBotChatPanel.tsx
@@ -5,4 +5,28 @@ import { useChat } from '~app/hooks/use-chat'
 import { BotId } from '../bots'
 import ConversationPanel from '../components/Chat/ConversationPanel'
 
-const MultiBotChatPane
+const MultiBotChatPanel: FC = () => {
+  const chatgptChat = useChat('chatgpt')
+  const bingChat = useChat('bing')
+
+  const generating = useMemo(
+    () => chatgptChat.generating || bingChat.generating,
+    [bingChat.generating, chatgptChat.generating],
+  )
+
+  const onUserSendMessage = useCallback(
+    (input: string, botId?: BotId) => {
+      if (botId === 'chatgpt') {
+        chatgptChat.sendMessage(input)
+      } else if (botId === 'bing') {
+        bingChat.sendMessage(input)
+      } else {
+        chatgptChat.sendMessage(input)
+        bingChat.sendMessage(input)
+      }
+    },
+    [bingChat, chatgptChat],
+  )
+
+  return (
+    <div className="grid grid-cols-2 grid-rows-[1fr_auto] overflow
