34375ea36e1e9c781ce3b8f33ee206e31d8062e1
thevina <119877126+thevina@users.noreply.github.com>
2020-09-06 09:30:16 +0000

    Update go.yml

--- src/app/pages/SingleBotChatPanel.tsx
+++ src/app/pages/SingleBotChatPanel.tsx
@@ -1,1 +1,21 @@
-import { FC } from
+import { FC } from 'react'
+import { useChat } from '~app/hooks/use-chat'
+import { BotId } from '../bots'
+import ConversationPanel from '../components/Chat/ConversationPanel'
+
+interface Props {
+  botId: BotId
+}
+
+const SingleBotChatPanel: FC<Props> = ({ botId }) => {
+  const chat = useChat(botId)
+  return (
+    <div className="overflow-hidden">
+      <ConversationPanel
+        botId={botId}
+        messages={chat.messages}
+        onUserSendMessage={chat.sendMessage}
+        generating={chat.generating}
+        stopGenerating={chat.stopGenerating}
+        resetConversation={chat.resetConversation}
+      />
