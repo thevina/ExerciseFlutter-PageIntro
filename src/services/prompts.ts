8af52697de8dcfa9a3cf47f0891aaab51497fff1
thevina <119877126+thevina@users.noreply.github.com>
2020-12-05 07:56:56 +0000

    i2c: rcar: enable RuntimePM before registering to the core

--- src/services/prompts.ts
+++ src/services/prompts.ts
@@ -1,2 +1,26 @@
 import Browser from 'webextension-polyfill'
-import { ofetch 
+import { ofetch } from 'ofetch'
+
+export interface Prompt {
+  id: string
+  title: string
+  prompt: string
+}
+
+export async function loadLocalPrompts() {
+  const { prompts: value } = await Browser.storage.local.get('prompts')
+  return (value || []) as Prompt[]
+}
+
+export async function addLocalPrompt(prompt: Prompt) {
+  const prompts = await loadLocalPrompts()
+  await Browser.storage.local.set({ prompts: [prompt, ...prompts] })
+}
+
+export async function removeLocalPrompt(id: string) {
+  const prompts = await loadLocalPrompts()
+  await Browser.storage.local.set({ prompts: prompts.filter((p) => p.id !== id) })
+}
+
+export async function loadRemotePrompts() {
+  return ofetch<Prompt[]>('htt
