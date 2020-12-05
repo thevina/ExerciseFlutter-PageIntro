1c5ab637059f4b558776ea9384e4d7b5f0d7a09a
thevina <119877126+thevina@users.noreply.github.com>
2020-12-05 13:47:35 +0000

    another

--- src/services/proxy-fetch.ts
+++ src/services/proxy-fetch.ts
@@ -12,4 +12,21 @@ import { streamAsyncIterable } from '~utils/stream-async-iterable'
 export function setupProxyExecutor() {
   // one port for one fetch request
   Browser.runtime.onConnect.addListener((port) => {
-    const abortControll
+    const abortController = new AbortController()
+    port.onDisconnect.addListener(() => {
+      abortController.abort()
+    })
+    port.onMessage.addListener(async (message: ProxyFetchRequestMessage) => {
+      console.debug('proxy fetch', message.url, message.options)
+      const resp = await fetch(message.url, {
+        ...message.options,
+        signal: abortController.signal,
+      })
+      port.postMessage({
+        type: 'PROXY_RESPONSE_METADATA',
+        metadata: {
+          status: resp.status,
+          statusText: resp.statusText,
+          headers: Object.fromEntries(resp.headers.entries()),
+        },
+      } as ProxyFetchResponseMetadataMessage)
