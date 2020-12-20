d112fb3e2171b9c14f86060610ba0aeddffdedb4
thevina <119877126+thevina@users.noreply.github.com>
2020-12-20 19:47:31 +0000

    0.15.0

--- src/services/user-config.ts
+++ src/services/user-config.ts
@@ -17,3 +17,20 @@ const userConfigWithDefaultValue = {
   openaiApiKey: '',
   openaiApiHost: 'https://api.openai.com',
   chatgptApiTemperature: 0.6,
+  startupPage: StartupPage.All,
+  bingConversationStyle: BingConversationStyle.Balanced,
+}
+
+export type UserConfig = typeof userConfigWithDefaultValue
+
+export async function getUserConfig(): Promise<UserConfig> {
+  const result = await Browser.storage.sync.get(Object.keys(userConfigWithDefaultValue))
+  return defaults(result, userConfigWithDefaultValue)
+}
+
+export async function updateUserConfig(updates: Partial<UserConfig>) {
+  console.debug('update configs', updates)
+  await Browser.storage.sync.set(updates)
+  for (const [key, value] of Object.entries(updates)) {
+    if (value === undefined) {
+      await Browser.st
